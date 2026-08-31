e12e74fddeaa0b202e073b8b9acfb153ea4135c2
ankhu1610 <chaubeyankit837@gmail.com>
2026-08-31 20:15:27 +0530

    Configure dynamic basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 const nextConfig = {
   output: 'export',
+  basePath: isGithubActions ? '/portfolio' : '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
